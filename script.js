ec423e0f0cd9243e156405b02f5ecca37d7e0adc
rahulr2402 <rrahul2405@gmail.com>
2024-10-08 15:59:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     const div = document.getElementById('contents');
-
     function updateContent() {
         fetch("https://raw.githubusercontent.com/rahulr2402/Learnings/main/meta.json")
             .then(resp => resp.json())
             .then(data => {
                 let innerContents = '';
-
                 data.learning_records.forEach(rec => {
-                        innerContents += `
-                                <div class="project">
-                                    <div class="first-data">
-                                        <img src="${rec.img}" alt="">
-                                        <div class="inner-data">
-                                            <span>${rec.title}</span>
-                                            <div class="buttons">
-                                                <button onclick="window.open('${rec.pagelink}', '_blank');">UI PAGE</button>
-                                                <button onclick="window.open('${rec.repolink}', '_blank');">SOURCE CODE</button>
-                                            </div>
-                                        </div>
+                    let buttons = '';
+                    if (rec.pagelink || rec.repolink) {
+                        if (rec.pagelink) {
+                            buttons += `<button onclick="window.open('${rec.pagelink}', '_blank');">UI PAGE</button>`;
+                        }
+                        if (rec.repolink) {
+                            buttons += `<button onclick="window.open('${rec.repolink}', '_blank');">SOURCE CODE</button>`;
+                        }
+                    } else if (rec.quizlink) {
+                        buttons += `<button onclick="window.open('${rec.quizlink}', '_blank');">QUIZ</button>`;
+                    }
+                    innerContents += `
+                        <div class="project">
+                            <div class="first-data">
+                                <img src="${rec.img}" alt="">
+                                <div class="inner-data">
+                                    <span>${rec.title}</span>
+                                    <div class="buttons">
+                                        ${buttons}
                                     </div>
-                                </div>`;
+                                </div>
+                            </div>
+                        </div>`;
                 });
-            div.innerHTML = innerContents;
-        })
-    };
+                div.innerHTML = innerContents;
+            });
+    }
     updateContent();
 });
